1cda28929d493026415744dde95c52c858331c29
Nakushnov81 <nakushnov@rambler.ru>
2023-02-22 15:34:31 +0400

    test

--- Modul_7/Modul_7.py
+++ Modul_7/Modul_7.py
@@ -3,5 +3,10 @@ from bs4 import BeautifulSoup
 
 page = requests.get('http://mfd.ru/currency/?currency=USD')
 soup = BeautifulSoup(page.text, 'html.parser')
-dollars = soup.find_all('td' in 'tr', {'class':'mfd-table mfd-currency-table'})
-print([dollar.text for dollar in dollars])
+dollars = soup.find('table', {'class':'mfd-table mfd-currency-table'})
+dollars = dollars.find_all('td')
+doll = ([dollar.text for dollar in dollars])
+change = []
+del doll[2::3]
+
+print(doll)
