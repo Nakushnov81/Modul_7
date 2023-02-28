6b0db08bb9191d84a493488c7dc1a0b03d7d9699
Nakushnov Anatoliy <nakushnov@rambler.ru>
2023-02-28 14:21:14 +0400

    Modul 7

--- Modul_7/Modul_7.py
+++ Modul_7/Modul_7.py
@@ -3,27 +3,30 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
+def get_data(dollars):
+    doll = ([dollar.text for dollar in dollars][-2:2:-3])
+    doll = [float(x) for x in doll]
+    dates = ([dollar.text[2:] for dollar in dollars][-3:2:-3])
+    return dates, doll
 
-page = requests.get('http://mfd.ru/currency/?currency=USD')
-soup = BeautifulSoup(page.text, 'html.parser')
-dollars = soup.find('table', {'class':'mfd-table mfd-currency-table'})
-dollars = dollars.find_all('td')
+def set_graf():
+    page = requests.get('http://mfd.ru/currency/?currency=USD')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    dollars = soup.find('table', {'class':'mfd-table mfd-currency-table'})
+    dollars = dollars.find_all('td')
 
-doll = ([dollar.text for dollar in dollars][-2:2:-3])
-doll = [float(x) for x in doll]
-dates = ([dollar.text[2:] for dollar in dollars][-3:2:-3])
+    dates, doll = get_data(dollars)
 
-print(doll)
-print(dates)
+    fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()
+    ax.xaxis.set_major_locator(MaxNLocator(12))
+    ax.grid()
 
-ax.xaxis.set_major_locator(MaxNLocator(12))
-ax.grid()
+    ax.set_title('Курс доллара', fontname="fantasy", fontsize=20)
+    ax.set_xlabel('Дата', fontsize=15, color='g')
+    ax.set_ylabel('USD', fontsize=15, color='g')
+    ax.plot(dates, doll, color= 'g')
 
-ax.set_title('Курс доллара', fontname="fantasy", fontsize=20)
-ax.set_xlabel('Дата', fontsize=15, color='g')
-ax.set_ylabel('USD', fontsize=15, color='g')
-ax.plot(dates, doll, color= 'g')
+    plt.show()
 
-plt.show()
+set_graf()
