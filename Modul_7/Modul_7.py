ceb5044253685c64409d693052219dd865d54e33
Nakushnov81 <nakushnov@rambler.ru>
2023-02-28 12:27:47 +0400

    Modul 7

--- Modul_7/Modul_7.py
+++ Modul_7/Modul_7.py
@@ -1,12 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 
 page = requests.get('http://mfd.ru/currency/?currency=USD')
 soup = BeautifulSoup(page.text, 'html.parser')
 dollars = soup.find('table', {'class':'mfd-table mfd-currency-table'})
 dollars = dollars.find_all('td')
-doll = ([dollar.text for dollar in dollars])
-change = []
-del doll[2::3]
+
+doll = ([dollar.text for dollar in dollars][-2:2:-3])
+doll = [float(x) for x in doll]
+dates = ([dollar.text[2:] for dollar in dollars][-3:2:-3])
 
 print(doll)
+print(dates)
+
+fig, ax = plt.subplots()
+
+ax.xaxis.set_major_locator(MaxNLocator(12))
+ax.grid()
+
+ax.set_title('Курс доллара', fontname="fantasy", fontsize=15)
+ax.set_xlabel('Дата', fontsize=12, color='g')
+ax.set_ylabel('USD', fontsize=12, color='g')
+ax.plot(dates, doll, color= 'g')
+
+plt.show()
